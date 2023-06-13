23e39d9dbae2e107721abfd322dbaa1e6ac23b57
mcbain666 <mrkeithdaly@gmail.com>
2023-06-13 19:34:32 +0100

    Main 6 commit

--- program/func_connections.py
+++ program/func_connections.py
@@ -4,13 +4,12 @@ from web3 import Web3
 from constants import (
   HOST,
   ETHEREUM_ADDRESS,
-  DYDX_API_KEY_TESTNET,
-  DYDX_API_SECRET_TESTNET,
-  DYDX_API_PASSPHRASE_TESTNET,
-  STARK_PRIVATE_KEY_TESTNET,
-  HTTP_PROVIDER_TESTNET,
+  DYDX_API_KEY,
+  DYDX_API_SECRET,
+  DYDX_API_PASSPHRASE,
+  STARK_PRIVATE_KEY,
+  HTTP_PROVIDER,
 )
-
 
 # Connect to DYDX
 def connect_dydx():
@@ -19,14 +18,14 @@ def connect_dydx():
   client = Client(
       host=HOST,
       api_key_credentials={
-          "key": DYDX_API_KEY_TESTNET,
-          "secret": DYDX_API_SECRET_TESTNET,
-          "passphrase": DYDX_API_PASSPHRASE_TESTNET,
+          "key": DYDX_API_KEY,
+          "secret": DYDX_API_SECRET,
+          "passphrase": DYDX_API_PASSPHRASE,
       },
-      stark_private_key=STARK_PRIVATE_KEY_TESTNET,
+      stark_private_key=STARK_PRIVATE_KEY,
       eth_private_key=config("ETH_PRIVATE_KEY"),
       default_ethereum_address=ETHEREUM_ADDRESS,
-      web3=Web3(Web3.HTTPProvider(HTTP_PROVIDER_TESTNET))
+      web3=Web3(Web3.HTTPProvider(HTTP_PROVIDER))
   )
 
   # Confirm client
